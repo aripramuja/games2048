5e80c446248a13a6f3d8895b57a6aa350a021947
elroyrn <elroyreinhard02@gmail.com>
2023-07-18 14:50:34 +0700

    2048 Mobile Games

---
+++ android/app/src/main/kotlin/com/example/mobile_games/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_games
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const/colors.dart
@@ -0,0 +1,50 @@
+import 'dart:ui';
+
+const backgroundColor = Color(0xffffffff);
+const textColor = Color(0xff00ADB5);
+const textColorWhite = Color(0xfff9f6f2);
+const boardColor = Color(0xff00ADB5);
+const emptyTileColor = Color(0xffFCE066);
+const buttonColor = Color(0xff00ADB5);
+const scoreColor = Color(0xff00ADB5);
+const overlayColor = Color.fromRGBO(238, 228, 218, 0.73);
+
+const color2 = Color(0xff85D9B4);
+const color4 = Color(0xff85D89F);
+const color8 = Color(0xff85D884);
+const color16 = Color(0xff86D9D3);
+const color32 = Color(0xff82BFD9);
+const color64 = Color(0xff89AEE8);
+const color128 = Color(0xff7B87DA);
+const color256 = Color(0xff7B87DA);
+const color512 = Color(0xff7B87DA);
+const color1024 = Color(0xff7B87DA);
+const color2048 = Color(0xffedc22e);
+
+const tileColors = {
+  2: color2,
+  4: color4,
+  8: color8,
+  16: color16,
+  32: color32,
+  64: color64,
+  128: color128,
+  256: color256,
+  512: color512,
+  1024: color1024,
+  2048: color2,
+};
+
+const tileImage = {
+  2: 'images/seed2.png',
+  4: 'images/smallsprout2.png',
+  8: 'images/sprout2.png',
+  16: 'images/sapling2.png',
+  32: 'images/tree2.png',
+  64: 'images/treeapple2.png',
+  128: 'images/apples.png',
+  256: 'images/apples2.png',
+  512: 'images/apples4.png',
+  1024: 'images/packapples.png',
+  2048: 'images/pieapple.png',
+};
